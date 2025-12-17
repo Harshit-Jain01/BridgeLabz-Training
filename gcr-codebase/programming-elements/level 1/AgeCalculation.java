f0939b5c3be5e97cc00cca02d4c0a7cebe26fb7e
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-17 22:38:13 +0530

    [Harshit Jain] Added of calculating age

---
+++ gcr-codebase/programming-elements/level 1/AgeCalculation.java
@@ -0,0 +1,8 @@
+class AgeCalculation{
+public static void main(String []args){
+//Declaring and assigning value to birthyear and currentyear
+int birthyear=2000,currentyear=2024;
+// Print the Harry Age
+System.out.println("Harry's age in 2024 is "+(currentyear-birthyear));
+}
+}
