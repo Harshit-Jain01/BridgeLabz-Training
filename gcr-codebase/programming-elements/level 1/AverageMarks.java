e7471e14ef43d4cc63d5cfae1b9ff1e1e73cc3a1
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-17 22:45:19 +0530

    [Harshit Jain] Added for finding the average marks

---
+++ gcr-codebase/programming-elements/level 1/AverageMarks.java
@@ -0,0 +1,11 @@
+class AverageMarks{
+public static void main(String []args){
+//Declaring the marks of Maths, Physics,Chemistry
+int Maths=94, Physics=95,Chemistry=96;
+//Calculating the average of PCM
+double averageMarks=(Maths+Physics+Chemistry)/3;
+//Displaying the Sam's average marks
+System.out.println("Sam's average mark in PCM is "+averageMarks);
+
+}
+}
