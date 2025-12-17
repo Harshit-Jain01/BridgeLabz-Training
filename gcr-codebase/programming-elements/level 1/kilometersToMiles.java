d7220be9f2d6e80264298ce5567dcdfec4e29e07
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-17 22:58:08 +0530

    [Harshit Jain] Added for converting to km to miles

---
+++ gcr-codebase/programming-elements/level 1/kilometersToMiles.java
@@ -0,0 +1,10 @@
+class  kilometersToMiles{
+    public static void main(String []args){
+        //Declaring distance in kilometer
+	double distanceInKilometer=10.8;
+	//converting kilometers to miles
+	double distanceInMiles=distanceInKilometer*1.6;
+	//Declaring distance in miles
+	System.out.println("The distance 10.8km in miles is "+distanceInMiles);
+  }
+}
