454f5a5b9c4f0c611a93650b9a1e58457d1e698a
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-18 13:10:45 +0530

    [Harshit Jain] Added for finding if person is eligible to vote or not

---
+++ gcr-codebase/control-flows/level 1/VotingEligibility.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class VotingEligibility{
+    public static void main(String []args){
+           //creating object of Scanner and taking  age as input from user
+	   Scanner input=new Scanner(System.in);
+           int age=input.nextInt();
+
+	   //checking if person's age  is greater than or equal to 18 or not and displaying result
+ 	   if(age>=18)
+	   System.out.println("The person's age is "+age+" and can vote");
+           else
+           System.out.println("The person's age is "+age+" and cannot vote");
+        }
+}
