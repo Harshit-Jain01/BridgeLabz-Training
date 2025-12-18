45990611a67648a9d57ea3aae07b7b0328b4dfea
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-18 19:00:07 +0530

    [Harshit Jain] Addded for making FizzBuzz program

---
+++ gcr-codebase/control-flows/level 2/FizzBuzz.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class FizzBuzz{
+    public static void main(String []args){
+           //creating object of Scanner and taking number from user
+	   Scanner input=new Scanner(System.in);
+           int number=input.nextInt();
+
+	   if(number<=0)
+	   System.out.println("Not a natural number");
+	   else
+	   {
+	   //applying for loop and checking condition
+	   for(int i=1;i<=number;i++){
+           if(i%3==0 && i%5==0)
+	   System.out.println("FizzBuzz");
+	   else if(i%3==0)
+           System.out.println("Fizz");
+           else if(i%5==0)
+           System.out.println("Buzz");
+           else
+           System.out.println(i);
+           }
+	}
+    }
+}
+	
