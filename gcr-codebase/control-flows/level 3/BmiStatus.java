76e776f23f2c97c343193ccf166df6808d82dd6a
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-18 23:54:22 +0530

    [Harshit Jain] Added for finding BMI status

---
+++ gcr-codebase/control-flows/level 3/BmiStatus.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class BmiStatus{
+    public static void main(String []args){
+           //creating object of Scanner and taking weight and height as input from user
+	   Scanner input=new Scanner(System.in);
+	   System.out.println("Enter weight and height");
+           double weight=input.nextDouble();
+	    double height=input.nextDouble();
+
+	  //calculating BMI
+	  double ht=height/100;
+	  double bmi=weight/(ht*ht);
+
+	  //displaying the BMI status
+	  if(bmi<=18.4)
+	  System.out.println("Underweight");
+	  else if(bmi>=18.5 && bmi<=24.9)
+	  System.out.println("Normal");
+	  else if(bmi>=25 && bmi<=39.9)
+	  System.out.println("Overweight");
+	  else
+          System.out.println("Obese");
+
+	 input.close();
+     }
+}
