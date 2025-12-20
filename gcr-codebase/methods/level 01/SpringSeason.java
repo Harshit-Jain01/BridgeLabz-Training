e5671d29e6c6966a157c9d139c67cd2ee3b5d574
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-20 12:41:55 +0530

    [Harshit Jain] Added for finding whether season is spring or not

---
+++ gcr-codebase/methods/level 01/SpringSeason.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class SpringSeason{
+
+  //creating method to find whether Its a Spring Season 
+   public  boolean checkSpringSeason(int month,int day){
+    if(day<=0 ||month<3 || month>6 || (month==3 && (day<20 || day>31) ) || (month==6 && day>20) ||(month==4 && day>30)||(month==5 && day>31))
+	return false;
+	return true;
+   }
+   
+   public static void main(String []args){
+   
+     //taking input for month and day
+	  Scanner input =new Scanner(System.in);
+      System.out.println("Enter month");
+	  int month=input.nextInt();
+	   System.out.println("Enter day");
+	  int day=input.nextInt();
+	  
+	  //creating object of class
+	  SpringSeason object=new SpringSeason();
+	  boolean result=object.checkSpringSeason(month,day);
+	  
+	  //displaying season
+	  if(result)
+	  System.out.println("Its a Spring Season");
+	  else 
+	  System.out.println("Its not a Spring Season");
+	   
+	  input.close();
+	  
+	  }
+   }
