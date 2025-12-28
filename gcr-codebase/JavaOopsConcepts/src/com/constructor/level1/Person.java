6208bc6c08c88d415145a1435c9622217231a8df
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-28 11:06:47 +0530

    [Harshit Jain]: added Person java file

---
+++ gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Person.java
@@ -0,0 +1,41 @@
+package com.constructor.level1;
+
+public class Person {
+	
+	//attributes
+	private String name;
+	private int age;
+	private double income;
+	
+	//parametrised constructor
+	public Person(String name, int age, double income) {
+		this.name = name;
+		this.age = age;
+		this.income = income;
+	}
+	public Person(Person p) {
+		this.name =p.name;
+		this.age =p.age;
+		this.income =p.income;
+		
+	}
+	
+	//display method
+	public void display() {
+		System.out.println("Name:"+name);
+		System.out.println("Age:"+age);
+		System.out.println("Income:"+income);
+	}
+public static void main(String[] args) {
+		
+		// creating object
+		Person p1=new Person("Amit",34,56004.0);
+		Person p2=new Person(p1);
+		System.out.println("person 1");
+		p1.display();
+		System.out.println("person 2");
+		p2.display();
+			
+	}
+
+}
