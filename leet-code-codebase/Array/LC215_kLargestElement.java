d85179956c1216f772e636ab84f233a5d5332b0f
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-30 19:00:55 +0530

    [Harshit Jain]: Added leetcode solution for K largest element problem

---
+++ leet-code-codebase/Array/LC215_kLargestElement.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class LC215_kLargestElement{
+
+    public static int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        int result = findKthLargest(nums, k);
+        System.out.println(k + "th largest element: " + result);
+
+        sc.close();
+    }
+}
