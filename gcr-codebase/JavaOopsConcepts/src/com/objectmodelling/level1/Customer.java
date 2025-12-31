ffb2ba34ba3c60f8f2877de0f5f743920ef55cf3
Harshit-Jain01 <jharshit554@gmail.com>
2025-12-31 22:35:30 +0530

    [Harshit Jain]: added UseBank java program files

---
+++ gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/level1/Customer.java
@@ -0,0 +1,29 @@
+package com.objectmodelling.level1;
+
+//Customer class
+class Customer {
+	private String name;
+    private double balance;
+    private Bank bank;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    // setters used for association
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // communication method
+    public void viewBalance() {
+        System.out.println("Customer: " + name);
+        System.out.println("Bank: " + bank.getBankName());
+        System.out.println("Balance: ₹" + balance);
+    }
+
+}
