d201e931c64c1dabb02af1733bb8d711b7edb43a
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-01 16:24:14 +0530

    [Harshit Jain]: added ecommerce java program files

---
+++ gcr-codebase/JavaOopsConcepts/src/com/encapsulation/ecommerce/Grocery.java
@@ -0,0 +1,14 @@
+package com.encapsulation.ecommerce;
+
+
+public class Grocery extends Product {
+
+    public Grocery(int productId, String name, double price) {
+        super(productId, name, price);
+    }
+
+    @Override
+    public double calculateDiscount() {
+        return getPrice() * 0.05; // 5% discount
+    }
+}
---
+++ gcr-codebase/JavaOopsConcepts/src/com/encapsulation/ecommerce/UseProduct.java
@@ -0,0 +1,42 @@
+package com.encapsulation.ecommerce;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UseProduct{
+
+    public static void main(String[] args) {
+
+        List<Product> products = new ArrayList<>();
+
+        products.add(new Electronics(101, "Laptop", 60000));
+        products.add(new Clothing(102, "Jacket", 3000));
+        products.add(new Grocery(103, "Rice Bag", 1200));
+
+        calculateFinalPrice(products);
+    }
+
+    // Polymorphism using Product reference
+    public static void calculateFinalPrice(List<Product> products) {
+
+        for (Product p : products) {
+
+            double tax = 0;
+
+            if (p instanceof Taxable) {
+                tax = ((Taxable) p).calculateTax();
+            }
+
+            double discount = p.calculateDiscount();
+            double finalPrice = p.getPrice() + tax - discount;
+
+            System.out.println("Product : " + p.getName());
+            System.out.println("Base Price : ₹" + p.getPrice());
+            System.out.println("Tax : ₹" + tax);
+            System.out.println("Discount : ₹" + discount);
+            System.out.println("Final Price : ₹" + finalPrice);
+            System.out.println("--------------------------");
+        }
+    }
+}
