bf03e4300622988fa24a4b6eb1ed1b81061a638b
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-01 12:58:45 +0530

    [Harshit Jain]: added smart home java program files

---
+++ gcr-codebase/JavaOopsConcepts/src/com/inheritance/single/smarthome/Device.java
@@ -0,0 +1,25 @@
+package com.inheritance.single.smarthome;
+
+public class Device {
+
+    private String deviceId;
+    private String status;
+
+    public Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
