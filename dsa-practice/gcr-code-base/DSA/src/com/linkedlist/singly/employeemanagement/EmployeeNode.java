dd8fe871fd599413113d6abfc12559d2202f1eb8
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-04 21:54:17 +0530

    [Harshit Jain]: added employee management java program files

--- dsa-practice/gcr-code-base/DSA/src/com/linkedlist/singly/employeemanagement/EmployeeNode.java
+++ dsa-practice/gcr-code-base/DSA/src/com/linkedlist/singly/employeemanagement/EmployeeNode.java
@@ -1,5 +1,59 @@
 package com.linkedlist.singly.employeemanagement;
 
 public class EmployeeNode {
+	    int id;
+	    String name;
+	    String department;
+	    double salary;
+	    EmployeeNode next;
+
+	    public EmployeeNode(int id, String name, String department, double salary) {
+	        this.id = id;
+	        this.name = name;
+	        this.department = department;
+	        this.salary = salary;
+	        this.next = null;
+	    }
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDepartment() {
+			return department;
+		}
+
+		public void setDepartment(String department) {
+			this.department = department;
+		}
+
+		public double getSalary() {
+			return salary;
+		}
+
+		public void setSalary(double salary) {
+			this.salary = salary;
+		}
+
+		public EmployeeNode getNext() {
+			return next;
+		}
+
+		public void setNext(EmployeeNode next) {
+			this.next = next;
+		}
+	    
 
 }
--- dsa-practice/gcr-code-base/DSA/src/com/linkedlist/singly/employeemanagement/LinkedList.java
+++ dsa-practice/gcr-code-base/DSA/src/com/linkedlist/singly/employeemanagement/LinkedList.java
@@ -2,4 +2,115 @@ package com.linkedlist.singly.employeemanagement;
 
 public class LinkedList {
 
+	EmployeeNode head;
+	
+	public void addAtBegining(int id, String name, String department, double salary) {
+		EmployeeNode p=new EmployeeNode(id,name,department,salary);
+		if(head==null){
+			head=p;
+			return;	
+		}
+		EmployeeNode temp=head;
+		p.setNext(temp);
+		head=p;	
+	}
+	
+	public void addAtLast(int id, String name, String department, double salary) {
+		EmployeeNode p=new EmployeeNode(id,name,department,salary);
+		if(head==null){
+			head=p;
+			return;	
+		}
+		EmployeeNode temp=head;
+		while(temp.getNext()!=null) {
+			temp=temp.getNext();
+		}
+		temp.setNext(p);
+	}
+	public void addAtPosition(int id, String name, String department, double salary,int pos) {
+		
+		EmployeeNode p=new EmployeeNode(id,name,department,salary);
+		
+		if (pos == 0) { // insert at beginning
+	        p.setNext(head);
+	        head = p;
+	        return;
+	    }
+		
+			EmployeeNode temp=head;
+			int count=0;
+			while(temp!=null && count<pos-1 ) {
+				count++;
+				temp=temp.getNext();
+			}
+			if (temp == null) {
+		        System.out.println("Invalid position");
+		        return;
+		    }
+			
+			p.setNext(temp.getNext());
+			temp.setNext(p);
+	}
+	
+	public void search(int id) {
+		if(head==null)return;
+
+		EmployeeNode temp=head;
+		while(temp!=null) {
+			if(temp.getId()==id) {
+				System.out.println("Employee details");
+				System.out.println("Id:"+temp.getId());
+				System.out.println("Name:"+temp.getName());
+				System.out.println("Dept:"+temp.getDepartment());
+				System.out.println("Salary:"+temp.getSalary());
+				return;
+			}
+			temp=temp.getNext();
+		}
+		
+			System.out.println("Id not available");
+	}
+	
+	public void delete(int id) {
+		if(head==null)return;
+		 if (head.getId() == id) {
+		        head = head.getNext();
+		        System.out.println(id + " Id deleted");
+		        return;
+		    }
+		EmployeeNode next=head;
+		EmployeeNode prev=next;
+		boolean flag=true;
+		while(next!=null) {
+			
+			if(next.getId()==id) {
+				prev.setNext(next.getNext());
+				System.out.println(id+"Id deleted");	
+				flag=false;
+				break;
+			}
+			prev=next;
+			next=next.getNext();
+		}
+		if(flag)
+			System.out.println("Id not available");		
+		
+	}
+	
+	
+	
+	public void display() {
+		if(head==null)
+		{
+			System.out.println("No data available");
+			return;
+		}
+		else {
+			EmployeeNode temp=head;
+			while(temp!=null) {
+				System.out.println(temp.getId()+","+temp.getName()+","+temp.getSalary()+","+temp.getDepartment());
+				temp=temp.getNext();
+			}
+		}
+	}
 }
