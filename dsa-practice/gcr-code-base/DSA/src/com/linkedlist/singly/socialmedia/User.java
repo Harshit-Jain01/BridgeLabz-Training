833a3eb83199e01f10bb45bcf3632945aad4f656
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-04 21:57:57 +0530

    [Harshit Jain]: added social media java program files

---
+++ dsa-practice/gcr-code-base/DSA/src/com/linkedlist/singly/socialmedia/User.java
@@ -0,0 +1,18 @@
+package com.linkedlist.singly.socialmedia;
+
+import java.util.*;
+
+public class User {
+	int userId;
+    String name;
+    int age;
+    List<Integer> friends;
+
+    User(int userId, String name, int age) {
+        this.userId = userId;
+        this.name = name;
+        this.age = age;
+        this.friends = new ArrayList<>();
+    }
+
+}
---
+++ dsa-practice/gcr-code-base/DSA/src/com/linkedlist/singly/socialmedia/UserNode.java
@@ -0,0 +1,12 @@
+package com.linkedlist.singly.socialmedia;
+
+public class UserNode {
+	 User user;
+	    UserNode next;
+
+	    UserNode(User user) {
+	        this.user = user;
+	        this.next = null;
+	    }
+
+}
