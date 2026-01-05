6cea9a1a9bad9682fd0452ec6586cf841c3673ed
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-05 11:17:00 +0530

    [Harshit Jain]: Added custom hash map java program

---
+++ dsa-practice/gcr-code-base/DSA/src/com/hashmapandhashfunction/CustomHashMap.java
@@ -0,0 +1,109 @@
+package com.hashmapandhashfunction;
+
+
+public class CustomHashMap {
+
+    // Node for linked list
+    static class Node {
+        int key;
+        int value;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private static final int CAPACITY = 16;
+    private Node[] buckets;
+    private int size;
+
+    public CustomHashMap() {
+        buckets = new Node[CAPACITY];
+        size = 0;
+    }
+
+    // Hash function
+    private int getIndex(int key) {
+        return key % CAPACITY;
+    }
+
+    // Insert or update
+    public void put(int key, int value) {
+        int index = getIndex(key);
+        Node head = buckets[index];
+
+        while (head != null) {
+            if (head.key == key) {
+                head.value = value; // update
+                return;
+            }
+            head = head.next;
+        }
+
+        Node newNode = new Node(key, value);
+        newNode.next = buckets[index];
+        buckets[index] = newNode;
+        size++;
+    }
+
+    // Retrieve value
+    public int get(int key) {
+        int index = getIndex(key);
+        Node head = buckets[index];
+
+        while (head != null) {
+            if (head.key == key) {
+                return head.value;
+            }
+            head = head.next;
+        }
+        return -1; // not found
+    }
+
+    // Remove key
+    public boolean remove(int key) {
+        int index = getIndex(key);
+        Node head = buckets[index];
+        Node prev = null;
+
+        while (head != null) {
+            if (head.key == key) {
+                if (prev == null) {
+                    buckets[index] = head.next;
+                } else {
+                    prev.next = head.next;
+                }
+                size--;
+                return true;
+            }
+            prev = head;
+            head = head.next;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+    
+    public static void main(String[] args) {
+
+        CustomHashMap map = new CustomHashMap();
+
+        map.put(1, 100);
+        map.put(2, 200);
+        map.put(1, 300); // update
+
+        System.out.println(map.get(1)); // 300
+        System.out.println(map.get(2)); // 200
+
+        map.remove(2);
+        System.out.println(map.get(2)); // -1
+    }
+    
+    
+    
+    
+}
