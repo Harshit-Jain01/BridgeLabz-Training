5b95cc34ccb29a2959714bd180491c7c49e9574b
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-05 11:19:12 +0530

    [Harshit Jain]: Added Pair with given sum java program

---
+++ dsa-practice/gcr-code-base/DSA/src/com/hashmapandhashfunction/PairWithGivenSum.java
@@ -0,0 +1,29 @@
+package com.hashmapandhashfunction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PairWithGivenSum {
+	
+    public static boolean hasPairWithSum(int[] arr, int target) {
+        Map<Integer, Boolean> map = new HashMap<>();
+
+        for (int num : arr) {
+            int required = target - num;
+
+            if (map.containsKey(required)) {
+                return true;
+            }
+
+            map.put(num, true);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        System.out.println(hasPairWithSum(arr, target));
+    }
+}
