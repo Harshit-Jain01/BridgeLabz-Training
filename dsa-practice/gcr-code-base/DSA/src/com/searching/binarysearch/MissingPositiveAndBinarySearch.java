d2e779e4331cd555186db6766e0582feec15bbfb
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-08 00:34:03 +0530

    [Harshit Jain]: added to find missing positive integer and find element using binary search

---
+++ dsa-practice/gcr-code-base/DSA/src/com/searching/binarysearch/MissingPositiveAndBinarySearch.java
@@ -0,0 +1,52 @@
+package com.searching.binarysearch;
+
+import java.util.Arrays;
+
+public class MissingPositiveAndBinarySearch {
+
+    public static int firstMissingPositive(int[] arr) {
+        int n = arr.length;
+        boolean[] visited = new boolean[n + 1];
+
+        for (int num : arr) {
+            if (num > 0 && num <= n) {
+                visited[num] = true;
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                return i;
+            }
+        }
+        return n + 1;
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, -1, 1};
+        int target = 4;
+
+        int missing = firstMissingPositive(arr);
+        System.out.println("First Missing Positive: " + missing);
+
+        Arrays.sort(arr);
+        int index = binarySearch(arr, target);
+        System.out.println("Index of target " + target + ": " + index);
+    }
+}
