53be065af4b8e24f0eeec30f95d6158e47ede0f8
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-10 22:15:10 +0530

    [Harshit Jain]: added library organiser java program files

--- dsa-practice/scenario-based/DSAScenarioProblems/src/com/day1/libraryorganiser/UseLibrary.java
+++ dsa-practice/scenario-based/DSAScenarioProblems/src/com/day1/libraryorganiser/UseLibrary.java
@@ -19,11 +19,6 @@ public class UseLibrary {
 		mystery.add(new Book("The Girl with the Dragon Tattoo", "Stieg Larsson", 14));
 		shelf.addGenre("Mystery", mystery);
 
-		List<Book> scienceFiction = new LinkedList<>();
-		scienceFiction.add(new Book("Dune", "Frank Herbert", 3));
-		scienceFiction.add(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 11));
-		shelf.addGenre("Science Fiction", scienceFiction);
-
 		List<Book> fantasy = new LinkedList<>();
 		fantasy.add(new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 9));
 		fantasy.add(new Book("The Hobbit", "J.R.R. Tolkien", 2));
