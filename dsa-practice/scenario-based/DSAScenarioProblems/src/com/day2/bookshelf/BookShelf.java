c0927d5a2bebb5c9d5cee3bb435d5fcd2f7019e3
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-12 23:46:12 +0530

    [Harshit Jain]: added bookshelf java program files

---
+++ dsa-practice/scenario-based/DSAScenarioProblems/src/com/day2/bookshelf/BookShelf.java
@@ -0,0 +1,44 @@
+package com.day2.bookshelf;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class BookShelf {
+		private String genre;
+		List<Book>list=new LinkedList<>();
+		Map<String,List<Book>>map=new HashMap<>();
+		
+		public void addGenre(String genre,List<Book>list) {
+			map.put(genre,list);
+		}
+		
+		public void borrow(String genre,String bookname) {
+			List<Book>booklist=map.get(genre);
+			for(Book b:booklist) {
+				if(b.getTitle().equalsIgnoreCase(bookname) ) {
+					if(b.getStock()>0) {
+					System.out.println("Book borrowed successfully");
+					b.setStock(b.getStock()-1);
+					return;	
+					}
+					else
+						System.out.println("Book out of stock");
+				}
+			}
+			System.out.println("Book not available");
+		}
+		
+		public void returnBook(String genre,String bookname) {
+			List<Book>booklist=map.get(genre);
+			for(Book b:booklist) {
+				if(b.getTitle().equalsIgnoreCase(bookname)) {
+					System.out.println("Book returned successfully");
+					b.setStock(b.getStock()+1);
+					return;	
+				}
+			}
+			System.out.println("Wrong Book");
+		}
+}
---
+++ dsa-practice/scenario-based/DSAScenarioProblems/src/com/day2/bookshelf/Library.java
@@ -0,0 +1,40 @@
+package com.day2.bookshelf;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Library {
+	 
+	public static void main(String []args) {
+		BookShelf shelf=new BookShelf();
+		
+		List<Book>fiction=new LinkedList<>();
+		fiction.add(new Book("The Great Gatsby","F. Scott Fitzgerald",10));
+		fiction.add(new Book("To Kill a Mockingbird","Harper Lee",13));
+		shelf.addGenre("Fiction", fiction);
+		
+		List<Book> mystery = new LinkedList<>();
+		mystery.add(new Book("Gone Girl", "Gillian Flynn", 2));
+		mystery.add(new Book("The Girl with the Dragon Tattoo", "Stieg Larsson", 14));
+		shelf.addGenre("Mystery", mystery);
+
+		List<Book> fantasy = new LinkedList<>();
+		fantasy.add(new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 9));
+		fantasy.add(new Book("The Hobbit", "J.R.R. Tolkien", 2));
+		shelf.addGenre("Fantasy", fantasy);
+		
+		shelf.borrow("Fantasy","The Hobbit");
+		shelf.borrow("Fantasy","The Hobbit");
+		shelf.borrow("Fantasy","The Hobbit");
+		shelf.borrow("Fantasy","Atomic Hobbit");
+		
+		shelf.returnBook("Fantasy","The Hobbit");
+		
+		
+		
+		
+		
+
+	}
+}
