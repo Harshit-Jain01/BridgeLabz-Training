58838e475d1a260457e6b442fbd3a35697804169
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-19 20:54:53 +0530

    [Harshit Jain]: added union and intersection of set java program

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/collectionsframework/set/UnionAndIntersection.java
@@ -0,0 +1,51 @@
+package com.collectionsframework.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UnionAndIntersection {
+	
+	public static  Set<Integer> union(Set<Integer>s1,Set<Integer>s2){
+		Set<Integer>s3=new HashSet<>();
+		for(int x:s1) {
+			s3.add(x);
+		}
+		for(int x:s2) {
+			s3.add(x);
+		}
+		return s3;
+	}
+	
+	public static  Set<Integer> intersection(Set<Integer>s1,Set<Integer>s2){
+		Set<Integer>s3=new HashSet<>();
+		for(int x:s1) {
+			if(s2.contains(x))
+			s3.add(x);
+		}
+		
+		return s3;
+	}
+	
+	
+	public static void main(String[] args) {
+		Set<Integer>s1=new HashSet<>();
+		s1.add(2);
+		s1.add(4);
+		s1.add(6);
+		s1.add(8);
+		System.out.println("Set1 "+s1);
+		Set<Integer>s2=new HashSet<>();
+		s2.add(4);
+		s2.add(2);
+		s2.add(10);
+		s2.add(30);
+		
+		System.out.println("Set2 "+s2);
+		
+		System.out.println("Union->"+UnionAndIntersection.union(s1, s2));
+		System.out.println("Intersection->"+UnionAndIntersection.intersection(s1, s2));
+
+		
+	}
+
+}
