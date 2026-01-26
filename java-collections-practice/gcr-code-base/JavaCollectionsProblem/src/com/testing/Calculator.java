fe92ad76b5061cdc4728789e7de490fbbf1314ad
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-26 23:45:06 +0530

    [Harshit Jain]: added Calculator java program

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsProblem/src/com/testing/Calculator.java
@@ -0,0 +1,20 @@
+package com.testing;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	
+	public int multiplication(int a, int b) {
+		return a*b;
+	}
+	
+	public int division(int a, int b) {
+		return a/b;
+	}
+}
