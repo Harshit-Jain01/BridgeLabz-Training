220b0add9a7301ffef8aa62839ab2a119d9b41d7
Harshit-Jain01 <jharshit554@gmail.com>
2026-01-29 00:04:53 +0530

    [Harshit Jain]: added health check pro scenario based problem

---
+++ java-iostreams-practice/scenario-based/IOStreamScenarioProblems/src/com/day1/healthcheckpro/PatientController.java
@@ -0,0 +1,12 @@
+package com.day1.healthcheckpro;
+
+public class PatientController {
+
+    @PublicAPI(description = "Register new patient")
+    public void registerPatient() {
+    }
+
+    @RequiresAuth(role = "ADMIN")
+    public void removePatient() {
+    }
+}
