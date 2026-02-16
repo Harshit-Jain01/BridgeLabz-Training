e4ab917e365f7bc39821c7b8897a2ecf35aa5211
Harshit-Jain01 <jharshit554@gmail.com>
2026-02-16 12:41:38 +0530

    [Harshit Jain]: added transaction management using jdbc

---
+++ dbms-jdbc-practice/gcr-code-base/JDBCConcepts/src/main/java/com/dbmsjdbc/transactionmanagement/Account.java
@@ -0,0 +1,28 @@
+package com.dbmsjdbc.transactionmanagement;
+
+public class Account {
+    private int accountId;
+    private String holderName;
+    private double balance;
+
+    public Account() {
+    }
+
+    public Account(int accountId, String holderName, double balance) {
+        this.accountId = accountId;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
